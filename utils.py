0fa6b69bda720eed00ef31dc9d93f3fa329efce4
Nils Meyer <nils.meyer@uni-a.de>
2023-03-13 22:05:29 +0100

    Prpare exercise 02.

--- utils.py
+++ utils.py
@@ -2,11 +2,15 @@ import matplotlib.pyplot as plt
 import torch
 
 
-def plot_contours(x, y, z, figsize=(4, 3), levels=25, title=None):
+def plot_contours(x, y, z, figsize=(4, 3), levels=25, title=None, paths=[]):
     with torch.no_grad():
         plt.figure(figsize=figsize)
         plt.contour(x, y, z, levels=levels, colors="k", linewidths=1)
         plt.contourf(x, y, z, levels=levels, cmap="plasma")
+        for path in paths:
+            xp = [p[0] for p in path]
+            yp = [p[1] for p in path]
+            plt.plot(xp, yp, "o-", linewidth=3)
         plt.xlabel("$x_1$")
         plt.ylabel("$x_2$")
         plt.title(title)
