bb3dbf3225e5c254f2ea0981a89b3e05367fb5ef
Nils Meyer <nils.meyer@uni-a.de>
2023-03-14 11:57:23 +0100

    Complete Exercise 2

--- utils.py
+++ utils.py
@@ -2,15 +2,19 @@ import matplotlib.pyplot as plt
 import torch
 
 
-def plot_contours(x, y, z, figsize=(4, 3), levels=25, title=None, paths=[]):
+def plot_contours(x, y, z, opti=[], figsize=(8, 6), levels=25, title=None, paths={}):
     with torch.no_grad():
         plt.figure(figsize=figsize)
+        # locator = ticker.LogLocator(subs=(0.5, 1.0))
         plt.contour(x, y, z, levels=levels, colors="k", linewidths=1)
         plt.contourf(x, y, z, levels=levels, cmap="plasma")
-        for path in paths:
+        for label, path in paths.items():
             xp = [p[0] for p in path]
             yp = [p[1] for p in path]
-            plt.plot(xp, yp, "o-", linewidth=3)
+            plt.plot(xp, yp, "o-", linewidth=3, label=label)
+            plt.legend()
+        if opti:
+            plt.plot(opti[0], opti[1], "ow")
         plt.xlabel("$x_1$")
         plt.ylabel("$x_2$")
         plt.title(title)
